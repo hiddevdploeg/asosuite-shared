244ba3bb2d3169874ad6c866bd88644742286edd
Niels Mouthaan <nielsmouthaan@me.com>
2022-11-29 15:14:57 +0100

    Added keyword model

---
+++ ASOSuiteShared/Model/Keyword.swift
@@ -0,0 +1,13 @@
+//
+//  Keyword.swift
+//  
+//
+//  Created by Niels Mouthaan on 29/11/2022.
+//
+
+import Foundation
+
+struct Keyword: Codable {
+    let keyword: String
+    let popularity: Int
+}
