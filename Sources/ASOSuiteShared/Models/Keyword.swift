f8033427bed555c4ed2a4a4da2a2e1def49736cc
Niels Mouthaan <nielsmouthaan@me.com>
2022-12-01 14:43:17 +0100

    no message

--- Sources/ASOSuiteShared/Models/Keyword.swift
+++ Sources/ASOSuiteShared/Models/Keyword.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-public struct Keyword: Codable, Equatable {
+public struct Keyword: Codable {
     
     public var keyword: String
     public var popularity: Int?
@@ -15,18 +15,5 @@ public struct Keyword: Codable, Equatable {
     public init(keyword: String, popularity: Int? = nil) {
         self.keyword = keyword.trimmingCharacters(in: .whitespacesAndNewlines).replacingOccurrences(of: ",", with: "")
         self.popularity = popularity
-    }
-    
-    public static func hasAllStatistics(keywords: [Keyword]) -> Bool {
-        for keyword in keywords {
-            if keyword.popularity == nil {
-                return false
-            }
-        }
-        return true
-    }
-    
-    public static func == (lhs: Keyword, rhs: Keyword) -> Bool {
-        return lhs.keyword == rhs.keyword
     }
 }
